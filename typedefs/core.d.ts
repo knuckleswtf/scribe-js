4c5761bddbaac526ed0d846b732aa986fe4a43f8
shalvah <diakon.ng@gmail.com>
2020-05-23 00:58:12 +0100

    Add internal typings

---
+++ typedefs/core.d.ts
@@ -0,0 +1,53 @@
+import {express} from "./express";
+
+export declare namespace endpoint {
+
+    interface ResponseField {
+        description: string,
+        type: 'string',
+    }
+
+    interface Parameter {
+        name: string,
+        description: string,
+        required: boolean
+        value: any
+    }
+
+    interface BodyParameter extends Parameter {
+        type: 'string',
+    }
+
+    type QueryParameter = Parameter
+
+    type UrlParameter = Parameter
+
+    interface Metadata {
+        groupName?: string
+        groupDescription?: string
+        title?: string
+        description?: string
+        authenticated?: boolean
+    }
+
+    export  interface Response {
+        status: number,
+        content: string
+    }
+
+    export interface Endpoint {
+        uri: string,
+        methods: string[],
+        metadata?: Metadata
+        headers?: {
+            [name: string]: string
+        }
+        bodyParameters?: BodyParameter[]
+        queryParameters?: QueryParameter[]
+        urlParameters?: UrlParameter[]
+        responses?: Response[]
+        responseFields?: ResponseField[]
+        route: express.Route,
+        handler: Function,
+    }
+}
