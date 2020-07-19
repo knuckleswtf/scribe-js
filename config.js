9abffe97ebdac2cae4e6fddcc66e5ee208749a9b
shalvah <diakon.ng@gmail.com>
2020-07-19 15:06:35 +0100

    Implement decorator pattern

--- config.js
+++ config.js
@@ -17,6 +17,11 @@ module.exports = {
         {
             include: ['*'],
             exclude: ['*.websocket'],
+            apply: {
+                headers: {
+
+                }
+            }
         }
     ],
     exampleLanguages: [
